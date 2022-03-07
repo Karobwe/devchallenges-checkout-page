2713cb9647616db6004612959f0501bd88ccbc63
Chanfi Attoumanni <attoumani.chanfi@gmail.com>
2022-03-07 01:44:08 +0100

    feat: move total price calculation logic, some unexpected behaviour have to be fixed

--- assets/js/product-quantity.js
+++ assets/js/product-quantity.js
@@ -5,8 +5,28 @@ export default function() {
         let add = product.querySelector('.quantity__add');
         let subtract = product.querySelector('.quantity__subtract');
         let quantity = product.querySelector('.quantity__value');
+        let title = product.parentNode.querySelector('.product__title');
+        let total = document.querySelector('.cart__total');
+        let shipping = document.querySelector('.cart__shipping');
 
-        if (typeof quantity != 'number') quantity.value = 0;
+        let productsData = {
+            bag: {
+                title: 'Vintage Backbag',
+                discountPrice: 54.99,
+                sellingPrice: 94.99,
+                quantity: 0,
+            },
+            shoes: {
+                title: 'Levi Shoesg',
+                discountPrice: 74.99,
+                sellingPrice: 124.99,
+                quantity: 0,
+            },
+            totalPrice: 0,
+            shipping: 19,
+        }
+
+        if (typeof quantity != 'number') quantity.value = 1;
 
         add.addEventListener('click', () => {
             let qte = parseInt(quantity.value);
@@ -17,6 +37,8 @@ export default function() {
             } else {
                 quantity.value = 1;
             }
+
+            updatePrices(title, productsData, quantity, shipping, total);
         });
 
         subtract.addEventListener('click', () => {
@@ -28,6 +50,35 @@ export default function() {
             } else {
                 quantity.value = 1;
             }
+
+            updatePrices(title, productsData, quantity, shipping, total);
         });
     });
+}
+
+/**
+ * Update total price according to selected quantity
+ * @param title
+ * @param productsData
+ * @param quantity
+ * @param shipping
+ * @param total
+ */
+let updatePrices = (title, productsData, quantity, shipping, total) => {
+    if (title.innerHTML === productsData.bag.title) {
+        productsData.bag.quantity = quantity.value;
+    } else if (title.innerHTML === productsData.shoes.title) {
+        productsData.shoes.quantity = quantity.value;
+    }
+
+    productsData.totalPrice = (productsData.shoes.discountPrice * productsData.shoes.quantity) + (productsData.bag.discountPrice * productsData.bag.quantity);
+
+    if(productsData.bag.quantity > 0 || productsData.shoes.quantity > 0)  {
+        productsData.totalPrice = productsData.totalPrice + productsData.shipping;
+        shipping.innerHTML = '$' + (productsData.shipping).toFixed(2);
+    } else {
+        shipping.innerHTML = '$' + (0).toFixed(2);
+    }
+
+    total.innerHTML = '$' + productsData.totalPrice.toFixed(2);
 }
